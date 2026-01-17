319424a3e5893c353a5a493431f90590a92b948e
TheMathWiz56 <146789934+TheMathWiz56@users.noreply.github.com>
2026-01-17 17:04:45 -0600

    Turret Skeleton

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,26 +24,14 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
   }
 
-  public static class IntakeConstants {
-    public static final int kIntakeMotorID = 0;
-    public static final double kIntakeMotorSpeed = 1.0;
+  public static class ShooterConstants {
+    public static final int kFlywheelMotorID = 10;
+    public static final int kHoodMotorID = 11;
+    public static final int kTurretMotorID = 12;
+
   }
 
-  public static class HopperConstants {
-    public static final int kBeltMotorID = 0;
-    public static final double kBeltMotorSpeed = 1.0;    
-    public static final int kRollerMotorID = 0;
-    public static final double kRollerMotorSpeed = 1.0;
-    public static final int kTransitionMotorID = 0;
-    public static final double kTransitionMotorSpeed = 1.0;    
-  }
-
-  public static class MagazineConstants {
-    public static final int kMagazineMotorID = 0;
-    public static final double kMagazineMotorSpeed = 1.0;
-  }
-
-  //** Just copied from 2025 project.*/
+  //**Just copied from 2025 project.*/
   public static class VisionConstants {
     public static class CropWindowSettings{
         @Getter @Setter private double cropXMin;
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,10 +1,71 @@
 package frc.robot.subsystems;
+import static frc.robot.Constants.ShooterConstants.*;
+import frc.robot.RangerHelpers;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+
+import lombok.Getter;
 
 public class Shooter extends SubsystemBase{
-    
+  private final TalonFX m_Flywheel, m_Turret, m_Hood;
+
+  private final VelocityVoltage m_FlywheelVV = new VelocityVoltage(0).withSlot(0);
+  private final DutyCycleOut m_FlywheelOut = new DutyCycleOut(0);
+
+  private final PositionVoltage m_TurretPV = new PositionVoltage(0).withSlot(0);
+
+  private final PositionVoltage m_HoodVoltage = new PositionVoltage(0).withSlot(0);
 
   public Shooter(){
+    m_Flywheel = new TalonFX(kFlywheelMotorID);
+    m_Turret = new TalonFX(kTurretMotorID);
+    m_Hood = new TalonFX(kHoodMotorID);
+
+    TalonFXConfiguration flywheelConfig = new TalonFXConfiguration();
+    flywheelConfig.Slot0.kS = 0;
+    flywheelConfig.Slot0.kV = 0;
+    flywheelConfig.Slot0.kP = 0;  
+    flywheelConfig.Slot0.kI = 0;
+    flywheelConfig.Slot0.kD = 0;
+    flywheelConfig.Voltage.withPeakForwardVoltage(Volts.of(8)).withPeakReverseVoltage(Volts.of(-8));
+    RangerHelpers.setupTalonFX(m_Flywheel, flywheelConfig);
+
+
+    TalonFXConfiguration turretConfig = new TalonFXConfiguration();
+    turretConfig.Slot0.kS = 0;
+    turretConfig.Slot0.kV = 0;
+    turretConfig.Slot0.kP = 0;
+    turretConfig.Slot0.kI = 0;
+    turretConfig.Slot0.kD = 0;
+    turretConfig.Voltage
+        .withPeakForwardVoltage(Volts.of(6))
+        .withPeakReverseVoltage(Volts.of(-6));
+    RangerHelpers.setupTalonFX(m_Turret, turretConfig);
+
+
+    TalonFXConfiguration hoodConfig = new TalonFXConfiguration();
+    hoodConfig.Slot0.kS = 0;
+    hoodConfig.Slot0.kV = 0;
+    hoodConfig.Slot0.kP = 0;
+    hoodConfig.Slot0.kI = 0;
+    hoodConfig.Slot0.kD = 0;
+    hoodConfig.Voltage
+        .withPeakForwardVoltage(Volts.of(6))
+        .withPeakReverseVoltage(Volts.of(-6));
+    RangerHelpers.setupTalonFX(m_Hood, hoodConfig);
+
+
 
   }
 
@@ -12,4 +73,37 @@ public class Shooter extends SubsystemBase{
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  private void setTurret(){
+
+
+  }
+
+  private void setHood(){
+
+
+  }
+
+  private void setFlywheel(int vel){
+
+
+  }
+
+  public void setTarget(){
+
+
+  }
+
+  public Command shoot(){
+    return holdState().alongWith(Commands.idle(this).onlyIf(() -> readyToShoot()).repeatedly());
+  }
+
+  public Command holdState(){
+    return null;
+  }
+
+  public boolean readyToShoot(){
+    return false;
+  }
+
 }
