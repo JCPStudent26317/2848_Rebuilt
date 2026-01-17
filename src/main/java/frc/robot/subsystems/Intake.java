00d2ec53072a55e1317cfed2a222ab1d14bc5f88
Jesuit Student 26317 <26317@jcpstudents.org>
2026-01-17 15:29:42 -0600

    Intake should be finished

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,21 +5,52 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import static frc.robot.Constants.IntakeConstants.*;
 
 public class Intake extends SubsystemBase {
     private final TalonFX intakeMotor = new TalonFX(kIntakeMotorID);
-    private final TalonFXConfiguration kIntakeMotorConfig = new TalonFXConfiguration();
+    private final TalonFXConfiguration intakeMotorConfig = new TalonFXConfiguration();
+    private final DutyCycleOut intakeOut = new DutyCycleOut(0.0);
 
     public Intake() {
-        // Configuration here
+        // Apply things to the configuration here
+
+        intakeMotor.getConfigurator().apply(intakeMotorConfig);
     }
 
     public Command holdState() {
-        return Commands.idle();
+        // setControl needs to run periodically at all times or the motor will disable I think
+        return Commands.run(() -> intakeMotor.setControl(intakeOut));
+    }
+
+    public void setMotorOutput(double output) {
+        intakeOut.Output = output;
+    }
+
+    public Command intake() {
+        return Commands.runOnce(() -> setMotorOutput(1.0));
+    }
+
+    public Command outtake() {
+        return Commands.runOnce(() -> setMotorOutput(-1.0));
+    }
+
+    public Command stop() {
+        return Commands.runOnce(() -> setMotorOutput(0.0));
+    }
+
+    // Put these on a whiletrue connected to a controller button
+    public Command manualIntake() {
+        return Commands.startRun(() -> setMotorOutput(1.0), () -> intakeMotor.setControl(intakeOut))
+        .finallyDo(() -> setMotorOutput(0.0));
+    }
+
+    public Command manualOuttake() {
+        return Commands.startRun(() -> setMotorOutput(-1.0), () -> intakeMotor.setControl(intakeOut))
+        .finallyDo(() -> setMotorOutput(0.0));
     }
 
 }
