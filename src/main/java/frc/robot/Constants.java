ccc6aed7ea3e006ee924d8d3571f35a4bff19476
TheMathWiz56 <146789934+TheMathWiz56@users.noreply.github.com>
2026-01-19 17:03:41 -0600

    Updated Limelight Constants for turret and new april tag counts

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -73,9 +73,9 @@ public final class Constants {
     public static final boolean kAddToPoseEstimator = true;
 
     // Camera settings
-    public static final int[] kRedAprilTagList = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    public static final int[] kBlueAprilTagList = new int[]{17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
-    public static final int[] kAllAprilTagList = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32};
+    public static final int[] kRedAprilTagList = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    public static final int[] kBlueAprilTagList = new int[]{17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+    public static final int[] kAllAprilTagList = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
     public static final float kDownscaleFactor = 4.0f;
     // crop settings
     public static final Map<String, CropWindowSettings> cameraCropWindowMap;
